ce6f7f4cdf3b883c0d089e5ac58306d3a196c739
Zara <zara@gene.co.uk>
2020-02-22 13:15:21 +0000

    install sass and first compile

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,10 @@
-var gulp = require('gulp)';
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+ 
+sass.compiler = require('node-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('./assets/scss/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./assets/test'));
+});
