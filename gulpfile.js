4c4d202f178866da749e4a901e29db397b84c3ef
Zara <zara@gene.co.uk>
2020-02-22 13:26:03 +0000

    install uglify and minimise the css

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var uglifycss = require('gulp-uglifycss');
  
 sass.compiler = require('node-sass');
  
 gulp.task('sass', function () {
   return gulp.src('./assets/scss/main.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(uglifycss({
+      "uglyComments": true
+    }))
     .pipe(gulp.dest('./assets/test'));
 });
