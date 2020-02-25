2a12c8c407fe1f7fa109ce724d9bb5f10cdbd73f
Zara <zara@gene.co.uk>
2020-02-25 19:41:18 +0000

    add autoprefixer and sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var uglifycss = require('gulp-uglifycss');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
  
 sass.compiler = require('node-sass');
  
 gulp.task('sass', function () {
   return gulp.src('./assets/scss/main.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.init())
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write())
     .pipe(uglifycss({
       "uglyComments": true
     }))
