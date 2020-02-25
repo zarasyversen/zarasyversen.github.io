302bbfba8f0f1a088af387bd3e299a0822a10f86
Zara <zara@gene.co.uk>
2020-02-25 21:35:49 +0000

    rewrite gulp4 style and sourcemaps works without uglifycss

--- gulpfile.js
+++ gulpfile.js
@@ -5,23 +5,27 @@ var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
  
 sass.compiler = require('node-sass');
- 
-gulp.task('sass', function () {
-  return gulp.src('./assets/scss/main.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(sourcemaps.init())
-    .pipe(autoprefixer())
-    .pipe(sourcemaps.write())
-    .pipe(uglifycss({
-      "uglyComments": true
-    }))
-    .pipe(gulp.dest('./assets/css'));
-});
 
-gulp.task('run', gulp.parallel('sass'));
+function style() {
+  
+    return (
+      gulp
+        .src('./assets/scss/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sourcemaps.identityMap())
+        .pipe(sass())
+        .on('error', sass.logError))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./assets/css')
+    );
+}
 
-gulp.task('watch', function() {
-    gulp.watch('./assets/scss/*.scss', gulp.parallel('sass'));
-});
+exports.style = style;
 
-gulp.task('default', gulp.parallel('run', 'watch'));
+// gulp.task('run', gulp.parallel('sass'));
+
+// gulp.task('watch', function() {
+//     gulp.watch('./assets/scss/*.scss', gulp.parallel('sass'));
+// });
+
+// gulp.task('default', gulp.parallel('run', 'watch'));
