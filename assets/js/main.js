55747531070ae72b5f9d07bfdd5e2435fe098bf7
Zara <zara@gene.co.uk>
2020-12-07 16:49:12 +0100

    content updated all on index

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,4 @@
-var menuActions = document.querySelectorAll('.js-nav-toggle');
+//
 
-function toggleMenu(){
-  const body = document.querySelector('body');
-  body.classList.toggle('nav-is-open');
-}
 
-for (var i = 0; i < menuActions.length; i++) {
-  menuActions[i].addEventListener('click', toggleMenu);
-}
+//
