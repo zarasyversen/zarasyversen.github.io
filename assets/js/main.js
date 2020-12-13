3333b9399389cf5391028e6b08b99341f2160dcd
Zara <zara@gene.co.uk>
2020-12-13 20:30:58 +0100

    backtotop

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,38 @@
-//
+/*eslint-disable*/
 
+const topButton = document.getElementById('go-top-button');
+let lastScrollTop = 0;
+let timer;
 
 //
+// Function to check if you are scrolling up or down.
+//
+function scrollFunction() {
+  if (timer) {
+    window.clearTimeout(timer);
+  }
+
+  timer = window.setTimeout(function() {
+		const currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+    if (currentScroll > 0 && lastScrollTop <= currentScroll) {
+      lastScrollTop = currentScroll;
+      topButton.classList.remove('is-active');
+    } else {
+      lastScrollTop = currentScroll;
+      topButton.classList.add('is-active');
+    }
+	}, 100);
+}
+
+// Add Event Listener for Scroll, everytime you scroll it runs this function
+window.addEventListener('scroll', scrollFunction);
+
+//
+// Function to return to the top of the page
+//
+function scrollToTop() {
+  document.documentElement.scrollTop = 0;
+}
+
+// Add Event Listener for topButton, click to go to the top.
+topButton.addEventListener('click', scrollToTop);
