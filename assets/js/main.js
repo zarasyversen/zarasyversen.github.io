c409fafe4f90a8b508c2c65339fac48221bfafc7
Zara <zara@gene.co.uk>
2020-12-29 16:44:47 +0100

    dark theme inline style tag to stop flash of default theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,14 +45,31 @@ topButton.addEventListener('click', scrollToTop);
 const colourSwitch = document.querySelector('#colour-switch');
 const body = document.querySelector('body');
 
+
+function checkTheme() {
+  if (localStorage.getItem("theme") === 'dark') {
+    body.classList.add('dark-mode');
+  }
+}
+
 function changeColourMode() {
+
+  // Remove style tag in head
+  const styleTag = document.head.querySelector('style');
+  if (styleTag) {
+    styleTag.remove();
+  }
 
   if (body.classList.contains('dark-mode')) {
     body.classList.remove('dark-mode');
+    localStorage.setItem('theme', 'light');
+
   } else {
     body.classList.add('dark-mode');
+    localStorage.setItem('theme', 'dark');
   }
-
 }
 
 colourSwitch.addEventListener('click', changeColourMode);
+
+checkTheme();
